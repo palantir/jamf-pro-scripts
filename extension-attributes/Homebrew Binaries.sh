0e11a084ac7d575f5020697729470fc67140f282
Mario Panighetti <mpanighetti@users.noreply.github.com>
2020-02-24 10:44:09 -0800

    fixed incorrect copyright

--- extension-attributes/Homebrew Binaries.sh
+++ extension-attributes/Homebrew Binaries.sh
@@ -11,7 +11,7 @@
 #         Version:  1.0
 #
 #
-# Copyright 2016 Palantir Technologies, Inc.
+# Copyright 2020 Palantir Technologies, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
