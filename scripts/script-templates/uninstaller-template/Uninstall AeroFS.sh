74b91bb6577fbe71daab2584ba8a2abbf8f9c929
Mario Panighetti <mpanighetti@mac.com>
2023-05-02 14:20:24 -0700

    made SC2034 ignore directive global
    - made SC2034 ignore directive global to account for unused $currentProcesses if removed from a child script

--- scripts/script-templates/uninstaller-template/Uninstall AeroFS.sh
+++ scripts/script-templates/uninstaller-template/Uninstall AeroFS.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# shellcheck disable=SC2034
 
 ###
 #
@@ -36,7 +37,6 @@
 # ENVIRONMENT VARIABLES (leave as-is)
 loggedInUser=$(/usr/bin/stat -f%Su "/dev/console")
 # For any file paths used later in this script, use "$loggedInUserHome" for the current user's home folder path. Don't just assume the home folder is at /Users/${loggedInUser}.
-# shellcheck disable=SC2034
 loggedInUserHome=$(/usr/bin/dscl . -read "/Users/${loggedInUser}" NFSHomeDirectory | /usr/bin/awk '{print $NF}')
 loggedInUserUID=$(/usr/bin/id -u "$loggedInUser")
 currentProcesses=$(/bin/ps aux)
