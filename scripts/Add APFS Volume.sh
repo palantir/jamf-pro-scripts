1afa3dddb9ae99da58b7a3e6a672e95bf5d3ffe4
Mario Panighetti <mpanighetti@palantir.com>
2023-10-24 12:50:36 -0700

    converted to POSIX Shell
    - converted to POSIX Shell
    - moved initial variables to script body to follow prerequisite functions (prevents stderr messages ahead of these checks)
    - moved check_volume ahead of check_startup_disk
    - removed $sizeSuffix (since it was standardized on GB anyway)
    - removed $volumeIdentifier (unused since this script was split from encryption workflows)

---
+++ scripts/Add APFS Volume.sh
@@ -0,0 +1,126 @@
+#!/bin/sh
+
+###
+#
+#            Name:  Add APFS Volume.sh
+#     Description:  Creates additional APFS volume at /Volumes/$newVolumeName (sharing disk space with other volumes in the startup disk container).
+#         Created:  2016-06-06
+#   Last Modified:  2023-10-24
+#         Version:  7.1
+#
+#
+# Copyright 2016 Palantir Technologies, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+###
+
+
+
+########## variable-ing ##########
+
+
+
+# Jamf Pro script parameter: "Startup Disk Name"
+startupDiskName="${4}"
+# Jamf Pro script parameter: "New Volume Name"
+newVolumeName="${5}"
+# Jamf Pro script parameter: "New Volume Size"
+# Enter a number of gigabytes.
+newVolumeSize="${6}"
+# Jamf Pro script parameter: "New Volume APFS Format"
+# See diskutil listFilesystems for expected formats.
+newVolumeAPFSFormat="${7}"
+
+
+
+########## function-ing ##########
+
+
+
+# Exits if any required Jamf Pro arguments are undefined.
+check_jamf_pro_arguments () {
+
+  if [ -z "$startupDiskName" ] || [ -z "$newVolumeName" ] || [ -z "$newVolumeSize" ] || [ -z "$newVolumeAPFSFormat" ]; then
+    echo "❌ ERROR: Undefined Jamf Pro argument, unable to proceed."
+    exit 74
+  fi
+
+}
+
+
+# Checks for presence of target startup disk.
+check_startup_disk () {
+
+  startupDiskCheck=$(/usr/sbin/diskutil info "$startupDiskName" 2>&1)
+  if echo "$startupDiskCheck" | grep -q "Could not find disk: ${startupDiskName}"; then
+    echo "❌ ERROR: Volume ${startupDiskName} missing, unable to proceed."
+    exit 72
+  fi
+
+}
+
+
+# Checks for presence of $newVolumeName.
+check_volume () {
+
+  volumeCheck=$(/usr/sbin/diskutil info "$newVolumeName" 2>&1)
+  if echo "$volumeCheck" | grep -v -q "Could not find disk: ${newVolumeName}"; then
+    echo "Volume ${newVolumeName} already present, no action required."
+    exit 0
+  fi
+
+}
+
+
+# Creates volume with specified name, format, and size in gigabytes (resizes startup disk for older filesystems).
+add_volume () {
+
+  /usr/sbin/diskutil \
+    apfs addVolume \
+    "$startupDiskDevice" \
+    "$newVolumeAPFSFormat" \
+    "$newVolumeName" \
+    -quota "${newVolumeSize}G"
+  echo "Volume ${newVolumeName} created (${newVolumeSize}G), formatted as ${newVolumeAPFSFormat}."
+  sleep 5
+
+}
+
+
+
+########## main process ##########
+
+
+
+# Verify system meets all script requirements (each function will exit if respective check determines that the script cannot be run).
+check_jamf_pro_arguments
+check_startup_disk
+check_volume
+
+
+# Add APFS volume.
+startupDiskInfo=$(/usr/sbin/diskutil info "$startupDiskName")
+fileSystemPersonality=$(echo "$startupDiskInfo" | /usr/bin/awk -F: '/File System Personality/ {print $NF}' | /usr/bin/sed 's/^ *//')
+if echo "$fileSystemPersonality" | /usr/bin/grep -q "APFS"; then
+  startupDiskDevice=$(echo "$startupDiskInfo" | /usr/bin/awk '/Part of Whole/ {print $4}')
+  add_volume
+else
+  echo "❌ ERROR: Unsupported file system (${fileSystemPersonality}), unable to proceed."
+  exit 1
+fi
+
+
+
+exit 0
